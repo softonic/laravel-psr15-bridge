1ebeeecfff258489ec25d016e048dc2a4e24a229
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2023-09-26 14:58:14 +0100

    Add support for laravel 10, phpunit 10, drop laravel 8. (#12)
    Add support for laravel 10, phpunit 10, drop laravel 8.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -3,19 +3,19 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__.'/src')
     ->in(__DIR__.'/tests')
 ;
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
         'concat_space' => ['spacing' => 'one'],
         'new_with_braces' => true,
         'no_blank_lines_after_phpdoc' => true,
-        'no_empty_phpdoc' => true,
         'no_empty_comment' => true,
+        'no_empty_phpdoc' => true,
         'no_leading_import_slash' => true,
-        'no_trailing_comma_in_singleline_array' => true,
+        'no_trailing_comma_in_singleline' => true,
         'no_unused_imports' => true,
-        'ordered_imports' => ['importsOrder' => null, 'sortAlgorithm' => 'alpha'],
+        'ordered_imports' => ['imports_order' => null, 'sort_algorithm' => 'alpha'],
         'phpdoc_add_missing_param_annotation' => ['only_untyped' => true],
         'phpdoc_align' => true,
         'phpdoc_no_empty_return' => true,
@@ -24,14 +24,13 @@ return PhpCsFixer\Config::create()
         'phpdoc_separation' => true,
         'phpdoc_to_comment' => true,
         'phpdoc_types' => true,
-        'psr0' => false,
-        'psr4' => true,
+        'psr_autoloading' => true,
         'return_type_declaration' => ['space_before' => 'none'],
-        'single_blank_line_before_namespace' => true,
+        'blank_lines_before_namespace' => true,
         'single_quote' => true,
         'space_after_semicolon' => true,
         'ternary_operator_spaces' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'trim_array_spaces' => true,
         'whitespace_after_comma_in_array' => true,
     ])
